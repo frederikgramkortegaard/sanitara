80dad11cbf8aa0e0b169381546e93da03b6777dd
frederikgram <frederikxyz@hotmail.com>
2020-08-02 23:07:50 +0200

    m

--- start_service.py
+++ start_service.py
@@ -1,17 +1,10 @@
 """ """
 import os
-from gooey import Gooey, GooeyParser
 
-@Gooey
-def start():
-    
-    parser = GooeyParser(description="My Cool GUI Program!") 
-    parser.add_argument('Filename', widget="FileChooser")
-    parser.add_argument('Date', widget="DateChooser")
+def main():
 
     os.chdir('./sanitara')
     os.system('uvicorn main:app --reload')
-    
 
 if __name__ == "__main__":
-    start()
+    main()
