3ce3e9a6bf7a76e7f0fe9db6ea905164bd0eb06e
Frederik Gram <36780843+frederikgram@users.noreply.github.com>
2021-03-05 11:58:21 +0100

    Update __init__.py

--- sanitara/processing/machines/__init__.py
+++ sanitara/processing/machines/__init__.py
@@ -9,7 +9,7 @@ from joblib import load
 def predict(model, url: str) -> float:
     """ Directly interfaces the model
         and returns its prediction as either
-        0.0 or 1.1
+        0.0 or 1.0
     """
 
     return float(model.predict([url])[0])
